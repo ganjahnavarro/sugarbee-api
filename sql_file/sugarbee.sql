44eb69dc28b372d3c2c242fb0362f936f841fd97
danicaobillos <danica.obillos@gmail.com>
2020-05-17 02:30:41 +0800

    updated database table structure

--- sql_file/sugarbee.sql
+++ sql_file/sugarbee.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2020 at 08:07 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+-- Generation Time: May 16, 2020 at 08:25 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,27 +34,25 @@ CREATE TABLE `orders` (
   `email` varchar(50) NOT NULL,
   `facebook` varchar(50) NOT NULL,
   `instagram` varchar(50) NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `deadline` timestamp NULL DEFAULT NULL,
   `pickup_location` varchar(120) NOT NULL,
   `delivery_method` varchar(30) NOT NULL,
-  `delevery_address` varchar(120) NOT NULL,
-  `order_name` varchar(50) NOT NULL,
+  `delivery_address` varchar(120) NOT NULL,
   `discount` float NOT NULL,
   `payment_status` tinyint(1) NOT NULL,
   `request` varchar(50) NOT NULL,
-  `special_offer` varchar(50) NOT NULL,
-  `quantity` int(11) NOT NULL
+  `special_offer` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`identifier`, `customer_name`, `contact_number`, `email`, `facebook`, `instagram`, `created_date`, `deadline`, `pickup_location`, `delivery_method`, `delevery_address`, `order_name`, `discount`, `payment_status`, `request`, `special_offer`, `quantity`) VALUES
-(1, 'Em Buenaventura', '9663601467', 'em@gmail.com', 'Em Buenaventura', 'embuenaventura', '2020-04-30 16:00:00', '2020-04-15 16:00:00', 'Magallanes', 'Lalamove', '11 Pureza St.', 'Small Tubs Sans Chips', 0, 0, 'note card', 'free cake', 25),
-(2, 'Dexmel Hernandez', '9663601467', 'dex@gmail.com', 'Dexmel Hernandez', 'dhernandez', '2020-03-29 16:00:00', '2020-04-16 16:00:00', 'Magallanes', 'Lalamove', 'Tondo Manila', 'Sansrival Chips', 0, 0, 'note card', 'free cake', 40),
-(3, 'Dex Mel', '9663601467', 'em@gmail.com', 'Dexmel Hernandez', 'embuenaventura', '2020-03-29 16:00:00', '2020-04-21 16:00:00', 'Magallanes', 'LAla', '3 Tondo Manila', 'Salted Caramel Cake', 0, 0, 'note card', 'free cake', 5);
+INSERT INTO `orders` (`identifier`, `customer_name`, `contact_number`, `email`, `facebook`, `instagram`, `created_date`, `deadline`, `pickup_location`, `delivery_method`, `delivery_address`, `discount`, `payment_status`, `request`, `special_offer`) VALUES
+(1, 'Em Buenaventura', '9663601467', 'em@gmail.com', 'Em Buenaventura', 'embuenaventura', '2020-04-30 16:00:00', '2020-04-15 16:00:00', 'Magallanes', 'Lalamove', '11 Pureza St.', 0, 0, 'note card', 'free cake'),
+(2, 'Dexmel Hernandez', '9663601467', 'dex@gmail.com', 'Dexmel Hernandez', 'dhernandez', '2020-03-29 16:00:00', '2020-04-16 16:00:00', 'Magallanes', 'Lalamove', 'Tondo Manila', 0, 0, 'note card', 'free cake'),
+(3, 'Dex Mel', '9663601467', 'em@gmail.com', 'Dexmel Hernandez', 'embuenaventura', '2020-03-29 16:00:00', '2020-04-21 16:00:00', 'Magallanes', 'LAla', '3 Tondo Manila', 0, 0, 'note card', 'free cake');
 
 -- --------------------------------------------------------
 
@@ -65,10 +62,9 @@ INSERT INTO `orders` (`identifier`, `customer_name`, `contact_number`, `email`, 
 
 CREATE TABLE `order_details` (
   `identifier` int(11) NOT NULL,
-  `item_name` varchar(50) NOT NULL,
+  `product_id` int(11) NOT NULL,
   `unit_price` float NOT NULL,
   `quantity` int(11) NOT NULL,
-  `total` float NOT NULL,
   `order_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -76,10 +72,10 @@ CREATE TABLE `order_details` (
 -- Dumping data for table `order_details`
 --
 
-INSERT INTO `order_details` (`identifier`, `item_name`, `unit_price`, `quantity`, `total`, `order_id`) VALUES
-(1, 'Splenda_Apple_Crumble', 1100, 2, 2200, 1),
-(2, 'Apple_Crumble', 800, 3, 2400, 1),
-(3, 'Carrot_Cake_-_6in', 550, 5, 2750, 2);
+INSERT INTO `order_details` (`identifier`, `product_id`, `unit_price`, `quantity`, `order_id`) VALUES
+(1, 1, 1100, 2, 1),
+(2, 2, 800, 3, 1),
+(3, 3, 550, 5, 2);
 
 -- --------------------------------------------------------
 
